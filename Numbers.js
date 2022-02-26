185a25ddb7b05f123eedcd33f023e64117cddb2b
BIPLOB <biplob2358@gmail.com>
2022-02-27 01:38:53 +0600

    added

--- Numbers.js
+++ Numbers.js
@@ -9,3 +9,10 @@ console.log(Number.parseInt(cc));
 console.log(Number.MAX_VALUE);
 console.log(Number.MIN_VALUE);
 console.log(Number.MIN_SAFE_INTEGER);
+
+var x = true;
+console.log(x);
+var y;
+console.log(y);
+var z = null;
+console.log(z);
