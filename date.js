046f55e94d383c2cae6e4406413ed6e88acfb2fe
BIPLOB <biplob2358@gmail.com>
2022-03-02 01:29:54 +0600

    add

---
+++ date.js
@@ -0,0 +1,13 @@
+var date = new Date();
+console.log(date);
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleTimeString());
+
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
