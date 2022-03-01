cf513cf9328a593d645ad4d4329d18ee7877b8ed
BIPLOB <biplob2358@gmail.com>
2022-03-02 01:13:18 +0600

    add

---
+++ math.js
@@ -0,0 +1,7 @@
+console.log(Math.round(Math.random() * 50));
+
+console.log(Math.sqrt(64));
+console.log(Math.max(1, 2, 3, 4, 5));
+console.log(Math.min(1, 2, 3, 4, 5));
+console.log(Math.E);
+console.log(Math.PI);
