d0acbb609654b2861cb60114b4d72ce2dab6d439
BIPLOB <biplob2358@gmail.com>
2022-03-03 19:18:08 +0600

    add

---
+++ else.js
@@ -0,0 +1,16 @@
+var a = 30;
+var b = 40;
+
+if (a > b) {
+  console.log("A is getter than B");
+} else {
+  console.log("B is getter than A");
+}
+
+var n = 10;
+
+if (n % 2 == 0) {
+  console.log("N is even number");
+} else {
+  console.log("B is odd number");
+}
---
+++ elseif.js
@@ -0,0 +1,20 @@
+var a = 40;
+var b = 50;
+
+if (a > b) {
+  console.log("A is getter than B");
+} else if (a == b) {
+  console.log("A and B are equal number");
+} else {
+  console.log("B is getter than A");
+}
+
+var n = 2;
+
+if (n == 0) {
+  console.log("The Number is zero");
+} else if (n % 2 == 0) {
+  console.log("N is erven");
+} else {
+  console.log("N is odd");
+}
