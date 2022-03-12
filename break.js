57e94bf7ea1df629fcfd30df48f2834e930c4077
BIPLOB <biplob2358@gmail.com>
2022-03-12 17:46:08 +0600

    add

---
+++ break.js
@@ -0,0 +1,17 @@
+while (true) {
+  var rand = Math.floor(Math.random() * 10 + 1);
+  if (rand == 9) {
+    console.log("Yeah You win!");
+    break;
+  } else {
+    console.log("You have got " + rand);
+  }
+}
+
+for (var i = 1; i < 10; i++) {
+  if (i % 5 == 0) {
+    break;
+  } else {
+    console.log(i);
+  }
+}
---
+++ nasted_loop.js
@@ -0,0 +1,8 @@
+var n = 5;
+for (var i = 1; i <= n; i++) {
+  var result = "";
+  for (var j = 1; j <= i; j++) {
+    result += j + " ";
+  }
+  console.log(result);
+}
