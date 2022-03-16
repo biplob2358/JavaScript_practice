95255a295e64cb37050918c356cf6ec25c8d85b0
BIPLOB <biplob2358@gmail.com>
2022-03-16 19:31:38 +0600

    add

---
+++ string_length.js
@@ -0,0 +1,14 @@
+var a = "Biplob Ghosh";
+var length = 0;
+
+while (true) {
+  if (a.charAt(length) == "") {
+    break;
+  } else {
+    length++;
+  }
+}
+console.log(length);
+
+var x = "Bangladesh";
+console.log(x.length);
