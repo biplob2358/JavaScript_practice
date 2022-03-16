24e191d3fc55f99514ca8d89fdecece1f74c5185
BIPLOB <biplob2358@gmail.com>
2022-03-16 19:02:31 +0600

    add

---
+++ String_Methods.js
@@ -0,0 +1,26 @@
+// Concat
+
+var a = "I am";
+var b = "Biplob Ghosh";
+var c = a.concat(" ", b);
+console.log(c);
+
+// substring
+// var d = c.substring(11);
+// console.log(d);
+console.log(c.charAt(0));
+console.log(c.charAt(6));
+console.log(c.startsWith("I"));
+console.log(c.endsWith("h"));
+
+// Lowercase/uppercase
+var x = "abc";
+console.log(x.toUpperCase());
+var y = "ABC";
+console.log(y.toLowerCase());
+//speace remove
+var w = "   xyebbciddfiods  ";
+console.log(w.trim());
+
+//split
+console.log(c.split(" "));
