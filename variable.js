e93d28fc641a641fb5c45ce8c198d2e30d143d9b
BIPLOB <biplob2358@gmail.com>
2022-03-25 18:27:34 +0600

    add

--- variable.js
+++ variable.js
@@ -4,3 +4,5 @@ var age = 22;
 console.log("Hello Everyone,I am " + name);
 console.log("I am " + age + " years old.");
 console.log(name + " is my birth name");
+console.log("Biplob Ghosh");
+console.log("DIU");
