21ed187f88aa0d97823573d413ee6af3864ca09a
BIPLOB <biplob2358@gmail.com>
2022-03-28 19:06:43 +0600

    add

---
+++ Array/method.js
@@ -0,0 +1,13 @@
+var arr = [4, 5, 9, 6];
+var arr2 = [4, 6, 8, 9];
+// console.log(arr.join(" "));
+
+// arr.fill(true);
+// console.log(arr);
+
+var arr3 = arr.concat(arr2);
+// console.log(arr3);
+console.log(Array.isArray(arr));
+
+arr4 = Array.from(arr);
+console.log(arr4);
