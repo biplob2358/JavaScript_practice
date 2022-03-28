ea1ec5deb6f723b1704cb0af9a90662b643f7e0f
BIPLOB <biplob2358@gmail.com>
2022-03-28 18:53:11 +0600

    add

--- Array/reverse_array.js
+++ Array/reverse_array.js
@@ -1,7 +1,9 @@
 var arr = [1, 2, 3, 4, 5, 6, 7];
-for (var i = 0; i < arr.length / 2; i++) {
-  var temp = arr[i];
-  arr[i] = arr[arr.length - 1 - i];
-  arr[arr.length - 1 - i] = temp;
-}
-console.log(arr);
+// for (var i = 0; i < arr.length / 2; i++) {
+//   var temp = arr[i];
+//   arr[i] = arr[arr.length - 1 - i];
+//   arr[arr.length - 1 - i] = temp;
+// }
+// console.log(arr);
+
+console.log(arr.reverse());
