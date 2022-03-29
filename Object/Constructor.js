1eb14b74a7f10363e944f6becad2c8f1f89d9ac8
BIPLOB <biplob2358@gmail.com>
2022-03-29 17:29:59 +0600

    add

---
+++ Object/Constructor.js
@@ -0,0 +1,14 @@
+var obj = Object();
+obj.x = 10;
+obj.y = 20;
+obj.z = 30;
+console.log(obj);
+
+//Another mathod
+
+var obj2 = new Object();
+obj2.a = 50;
+obj2.b = 60;
+obj2.c = 70;
+console.log(obj2);
+console.log(typeof obj2);
---
+++ Object/Literal .js
@@ -0,0 +1,7 @@
+var obj = {
+  x: 10,
+  y: 20,
+};
+obj.z = 30;
+
+console.log(obj);
