7fddd48eb2d300c1e7d1d9eff2201e7fb9f2697e
BIPLOB <biplob2358@gmail.com>
2022-03-30 12:13:01 +0600

    add

---
+++ Function/Invoking .js
@@ -0,0 +1,21 @@
+function functionName() {
+  console.log("I am Biplob");
+}
+
+function add() {
+  var a = 50;
+  var b = 30;
+  var c = a + b;
+  console.log("The sum of " + a + " + " + b + " = " + c);
+}
+
+function sub() {
+  var a = 50;
+  var b = 30;
+  var c = a - b;
+  console.log("The sum of " + a + " - " + b + " = " + c);
+}
+
+functionName();
+add();
+sub();
