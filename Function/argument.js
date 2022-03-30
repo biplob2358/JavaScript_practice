020cfa186b5a87ffa688ed656a46aff80af5add4
BIPLOB <biplob2358@gmail.com>
2022-03-30 12:47:48 +0600

    add

---
+++ Function/argument.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+  var result = a + b;
+  console.log(result);
+}
+
+function sub(a, b) {
+  var result = a - b;
+  console.log(result);
+}
+
+add(2, 4);
+add(234, 400);
+sub(10, 7);
---
+++ Function/sumOfArray.js
@@ -0,0 +1,15 @@
+arr1 = [12, 14, 34];
+arr2 = [34, 54, 66];
+arr3 = [33, 44, 55];
+
+function sumOfArray(arr) {
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  console.log(sum);
+}
+
+sumOfArray(arr1);
+sumOfArray(arr2);
+sumOfArray(arr3);
