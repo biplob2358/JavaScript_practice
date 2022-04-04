8bc748ce30c96e5b4fccd36986e7323eb515c144
BIPLOB <biplob2358@gmail.com>
2022-04-04 13:04:46 +0600

    add

---
+++ Function/expresion.js
@@ -0,0 +1,12 @@
+setTimeout(function () {
+  console.log("Biplob Ghosh");
+}, 5000);
+
+//example
+
+var addition = function (a, b) {
+  return a + b;
+};
+
+var another = addition;
+console.log(another(3, 3));
